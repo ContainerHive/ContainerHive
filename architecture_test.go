5f33a0201fa9ec067d9add9b6283a86a9fb60115
Timo Reymann <mail@timo-reymann.de>
2026-03-08 17:17:15 +0100

    test: Add architecture tests (#26)
    * test: Add architecture tests
    
    * chore: Add archunit to LICENSE

---
+++ architecture_test.go
@@ -0,0 +1,27 @@
+package containerhive_test
+
+import (
+	"testing"
+)
+
+func TestPackageDirections(t *testing.T) {
+	checks := []struct {
+		err         error
+		description string
+	}{
+		{
+			cmdLayer.ShouldNotReferLayers(internalLayer),
+			"cmd is not allowed to reference internal",
+		},
+		{
+			pkgLayer.ShouldNotReferLayers(cmdLayer),
+			"pkg should not refer to internal",
+		},
+	}
+
+	for _, check := range checks {
+		if check.err != nil {
+			t.Fatal(check.err, check.description)
+		}
+	}
+}
